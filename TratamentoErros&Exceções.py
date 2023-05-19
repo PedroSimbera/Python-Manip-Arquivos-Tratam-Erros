06b29aee5b6515e774bfca34131297c647c8b2a6
PedroSimbera <pedrosimbera@hotmail.com>
2023-05-19 20:42:56 -0300

    rename arquivo e sobre erros

---
+++ TratamentoErros&Exceções.py
@@ -0,0 +1,2 @@
+# Falhas são diferentes de erros, pois os erros são pontenciais erros de semânticas das lingaguens. Já as falhas são comportamentos do algoritmo diferente daquele que foi previsto pelo programador.
+
